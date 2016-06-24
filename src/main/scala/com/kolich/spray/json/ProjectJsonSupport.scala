319335d7a0e86597d43a2c29f7d247e6a1f3fc76
U454343 <kingdom1>
2016-06-24 17:16:23 -0600

    update

--- src/main/scala/com/kolich/spray/json/ProjectJsonSupport.scala
+++ src/main/scala/com/kolich/spray/json/ProjectJsonSupport.scala
@@ -1,16 +1,10 @@
 package com.kolich.spray.json
 
-import spray.json.DefaultJsonProtocol
+import spray.json._
 import spray.httpx.SprayJsonSupport
-import spray.json.JsonFormat
-import spray.json.JsNumber
-import spray.json.JsString
-import spray.json.JsValue
-import com.kolich.spray.domain.Project;
-import com.kolich.spray.domain.types.Tier
-import com.kolich.spray.domain.types.Tier1
-import com.kolich.spray.domain.types.Tier2
-import com.kolich.spray.domain.types.Tier3
+import com.kolich.spray.domain._
+;
+import com.kolich.spray.domain.types._
 
 
 /**
@@ -22,10 +16,54 @@ object ProjectJsonSupport extends DefaultJsonProtocol with SprayJsonSupport {
     def read(value: JsValue) : Tier = DefaultJsonProtocol.StringJsonFormat.read(value) match { case "tier1" => new Tier1 case "tier2" => new Tier2 case "tier3" => new Tier3}
   }
 
+  implicit object MyStatusJsonFormat extends JsonFormat[com.kolich.spray.domain.types.Status] {
+    def write(x: Status) = JsString(x match { case i:IsDefined => "IsDefined" case i:InProgress => "InProgress" case i:InTesting => "InTesting" case i:IsComplete => "IsComplete" case i:InAccepted => "InAccepted" case i:IsReleased => "IsReleased" case i:IsClosed => "IsClosed" case i:IsDead => "IsDead"})
+    def read(value: JsValue) : Status = DefaultJsonProtocol.StringJsonFormat.read(value) match { case "IsDefined" => new IsDefined case "InProgress" => new InProgress case "InTesting" => new InTesting  case "IsComplete" => new IsComplete case "InAccepted" => new InAccepted  case "IsReleased" => new IsReleased case "IsClosed" => new IsClosed}
+  }
+
+  implicit object MyStoryPointsJsonFormat extends JsonFormat[com.kolich.spray.domain.types.StoryPoints] {
+    def write(x: StoryPoints) = JsString(x match { case i:HalfPoint => "HalfPoint" case i:OnePoint => "OnePoint" case i:TwoPoints => "TwoPoints" case i:ThreePoints => "ThreePoints" case i:FivePoints => "FivePoints" case i:EightPoints => "EightPoints" case i:Hugh => "Hugh"})
+    def read(value: JsValue) : StoryPoints = DefaultJsonProtocol.StringJsonFormat.read(value) match { case "HalfPoint" => new HalfPoint case "OnePoint" => new OnePoint case "TwoPoints" => new TwoPoints case "ThreePoints" => new ThreePoints case "FivePoints" => new FivePoints case "EightPoints" => new EightPoints}
+  }
+
   implicit object MyDateJsonFormat extends JsonFormat[java.util.Date] {
     def write(x: java.util.Date) = JsNumber(if (x != null) x.getTime else System.currentTimeMillis)
     def read(value: JsValue) = new java.util.Date(DefaultJsonProtocol.LongJsonFormat.read(value))
   }
 
+  implicit object AnimalJsonFormat extends RootJsonFormat[Story] {
+    def write(a: Story) = a match {
+      case p: UserStory => p.toJson
+      case p: ShortStory => p.toJson
+    }
+    def read(value: JsValue) =
+    // If you need to read, you will need something in the
+    // JSON that will tell you which subclass to use
+      value.asJsObject.fields("kind") match {
+        case JsString("userstory") => value.convertTo[UserStory]
+        case JsString("shortstory") => value.convertTo[ShortStory]
+      }
+  }
+
+
+  implicit val projectFormats9 = jsonFormat2(User)
+
+  implicit val projectFormats3 = jsonFormat7(Theme)
+
+  implicit val projectFormats2 = jsonFormat5(CoverStory)
+
+  implicit val projectFormats8 = jsonFormat5(Release)
+
+  implicit val projectFormats7 = jsonFormat5(Iteration)
+
+  implicit val projectFormats6 = jsonFormat14(Epic)
+
+  implicit val projectFormats5 = jsonFormat15(ShortStory)
+
+  implicit val projectFormats4 = jsonFormat15(UserStory)
+
   implicit val projectFormats = jsonFormat13(Project)
+
+
+
 }

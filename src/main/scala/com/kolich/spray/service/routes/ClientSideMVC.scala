01d70b6e971a4aae9a50639cde30b8dcb1a1fe28
U454343 <kingdom1>
2016-06-24 17:20:38 -0600

    update

--- src/main/scala/com/kolich/spray/service/routes/ClientSideMVC.scala
+++ src/main/scala/com/kolich/spray/service/routes/ClientSideMVC.scala
@@ -1,8 +1,9 @@
 package com.kolich.spray.service.routes
 
+import com.kolich.spray.domain.User
 import com.kolich.spray.service.Service
 import com.kolich.spray.templating.ScalateSupport
-
+import com.kolich.spray.json.ProjectJsonSupport._
 
 /**
   * Created by romeo on 6/22/2016.
@@ -12,8 +13,15 @@ class ClientSideMVC  extends Service with ScalateSupport {
   override def receive = runRoute {
     path("main") {
       get {
-        render("WEB-INF/ssp/main.ssp")
+        render("templates/main.ssp")
       }
-    }
+    }  ~
+      path("testuser") {
+       get {
+         complete {
+           User("u454343", "Tony Ford")
+         }
+       }
+     }
   }
 }
